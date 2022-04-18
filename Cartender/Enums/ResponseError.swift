e7501ab3bf10c02d89e95041626725acaa6b0f02
Paul Dippold <pauldippold@me.com>
2022-04-18 11:12:05 -0400

    Ignore

---
+++ Cartender/Enums/ResponseError.swift
@@ -0,0 +1,34 @@
+//
+//  ResponseError.swift
+//  Cartender
+//
+//  Created by Paul Dippold on 4/15/22.
+//
+
+import Foundation
+
+enum ResponseError: Int {
+    case sleepMode = 1155
+    case anotherCommand = 1125
+    case lowCoverage = 1132
+    case doorsOpen = 1131
+    case invalidSession = 1003
+    case logout = 1001
+    
+    var message: String {
+        switch self {
+        case .sleepMode:
+            return "Your car is in sleep mode. Turn it on and then try again."
+        case .anotherCommand:
+            return "Another remote command is being executed. Please wait for it to finish and then try again"
+        case .lowCoverage:
+            return "Your car is not responding. Turn it on and then try again."
+        case .doorsOpen:
+            return "Your car doors may be open. Close them and then try again."
+        case .invalidSession:
+            return "Login and then try again."
+        case .logout:
+            return "Login and then try again."
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+target 'Cartender' do
+	use_frameworks!
+  pod 'SwiftyBeaver'
+	pod 'SwiftHTTP'
+  pod 'SwiftKeychainWrapper'
+  pod 'NotificationBannerSwift'
+end
+
+target 'CartenderIntents' do
+  use_frameworks!
+  pod 'SwiftyBeaver'
+  pod 'SwiftHTTP'
+  pod 'SwiftKeychainWrapper'
+end
+
+target 'CartenderIntentsUI' do
+  use_frameworks!
+  pod 'SwiftyBeaver'
+  pod 'SwiftHTTP'
+  pod 'SwiftKeychainWrapper'
+end
